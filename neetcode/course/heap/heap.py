68432861b9c85746b63468824af92d432cf3fcea
Aleksandr Stserbatski <astserba@gmail.com>
2024-06-03 19:11:10 +0300

    MinHeap

--- neetcode/course/heap/heap.py
+++ neetcode/course/heap/heap.py
@@ -1,105 +1,81 @@
-# Python3 implementation of Min Heap
-
-import sys
-
-
 class MinHeap:
+    def __init__(self):
+        self.heap = [0]  # Skip first element
 
-    def __init__(self, maxsize):
-        self.maxsize = maxsize
-        self.size = 0
-        self.Heap = [0] * (self.maxsize + 1)
-        self.Heap[0] = -1 * sys.maxsize
-        self.FRONT = 1
+    def push(self, val):
+        self.heap.append(val)  # Simply append node to the end of array
+        i = len(self.heap) - 1  # Assign Current node pointer to New node - Last in Array
 
-    # Function to return the position of parent for the node currently at pos
-    def parent(self, pos):
-        return pos // 2
+        # Percolate up - (move current up if lower than parents)
+        while self.heap[i] < self.heap[i // 2]:
+            # Current node is lower than Parent node -> swap Current with Parent -> move Up
+            self.heap[i], self.heap[i // 2] = self.heap[i // 2], self.heap[i]
+            i = i // 2
 
-    # Function to return the position of the left child for the node currently at pos
-    def leftChild(self, pos):
-        return 2 * pos
+    def pop(self):
+        if len(self.heap) == 1:
+            return None
+        if len(self.heap) == 2:
+            return self.heap.pop()
 
-    # Function to return the position of the right child for the node currently at pos
-    def rightChild(self, pos):
-        return (2 * pos) + 1
+        min_item = self.heap[1]
 
-    # Function that returns true if the passed node is a leaf node
-    def isLeaf(self, pos):
-        return pos * 2 > self.size
+        self.heap[1] = self.heap.pop()  # Move last item to the root
+        self.percolate_down(1)
+        return min_item
 
-    # Function to swap two nodes of the heap
-    def swap(self, fpos, spos):
-        self.Heap[fpos], self.Heap[spos] = self.Heap[spos], self.Heap[fpos]
+    def heapify(self, arr):
+        # 0th position is moved to the end
+        arr.append(arr[0])
 
-    # Function to heapify the node at pos
-    def minHeapify(self, pos):
-        # If the node is a non-leaf node and greater
-        # than any of its child
-        if not self.isLeaf(pos):
-            if (self.Heap[pos] > self.Heap[self.leftChild(pos)] or
-                    self.Heap[pos] > self.Heap[self.rightChild(pos)]):
+        self.heap = arr
+        cur = (len(self.heap) - 1) // 2
+        while cur > 0:
+            self.percolate_down(cur)
+            cur -= 1
 
-                # Swap with the left child and heapify
-                # the left child
-                if self.Heap[self.leftChild(pos)] < self.Heap[self.rightChild(pos)]:
-                    self.swap(pos, self.leftChild(pos))
-                    self.minHeapify(self.leftChild(pos))
+    def percolate_down(self, i):
+        while 2 * i < len(self.heap):  # loop until have left child (not leaf)
+            if self.currentHasRightChild(i) and self.rightChildLowerThanLeft(i) and self.rightChildLowerThanCurrent(i):
+                # Swap Current with Right child - move current node Down
+                self.heap[2 * i + 1], self.heap[i] = self.heap[i], self.heap[2 * i + 1]
+            elif self.leftChildLowerThanCurrent(i):
+                # Swap Current with Left child - move current node Down
+                self.heap[2 * i], self.heap[i] = self.heap[i], self.heap[2 * i]
+            else:
+                # Current node is lower than its children
+                break
 
-                # Swap with the right child and heapify
-                # the right child
-                else:
-                    self.swap(pos, self.rightChild(pos))
-                    self.minHeapify(self.rightChild(pos))
+    def currentHasRightChild(self, i):
+        return 2 * i + 1 < len(self.heap)
 
-    # Function to insert a node into the heap
-    def insert(self, element):
-        if self.size >= self.maxsize:
-            return
-        self.size += 1
-        self.Heap[self.size] = element
+    def rightChildLowerThanLeft(self, i):
+        return self.heap[2 * i + 1] < self.heap[2 * i]
 
-        current = self.size
+    def rightChildLowerThanCurrent(self, i):
+        return self.heap[2 * i + 1] < self.heap[i]
 
-        while self.Heap[current] < self.Heap[self.parent(current)]:
-            self.swap(current, self.parent(current))
-            current = self.parent(current)
-
-    # Function to print the contents of the heap
-    def Print(self):
-        for i in range(1, (self.size // 2) + 1):
-            print(" PARENT :  " + str(self.Heap[i]) + " LEFT CHILD :  " +
-                  str(self.Heap[2 * i]) + " RIGHT CHILD :  " +
-                  str(self.Heap[2 * i + 1]))
-
-    # Function to build the min heap using the minHeapify function
-    def minHeap(self):
-        for pos in range(self.size // 2, 0, -1):
-            self.minHeapify(pos)
-
-    # Function to remove and return the minimum element from the heap
-    def remove(self):
-        popped = self.Heap[self.FRONT]
-        self.Heap[self.FRONT] = self.Heap[self.size]
-        self.size -= 1
-        self.minHeapify(self.FRONT)
-        return popped
+    def leftChildLowerThanCurrent(self, i):
+        return self.heap[2 * i + 1] < self.heap[i]
 
 
-# Driver Code
 if __name__ == "__main__":
-    print('The minHeap is ')
-    minHeap = MinHeap(15)
-    minHeap.insert(5)
-    minHeap.insert(3)
-    minHeap.insert(17)
-    minHeap.insert(10)
-    minHeap.insert(84)
-    minHeap.insert(19)
-    minHeap.insert(6)
-    minHeap.insert(22)
-    minHeap.insert(9)
-    minHeap.minHeap()
+    minHeap = MinHeap()
+    minHeap.push(15)
+    minHeap.push(5)
+    minHeap.push(3)
+    minHeap.push(17)
+    minHeap.push(10)
+    minHeap.push(84)
+    minHeap.push(19)
+    minHeap.push(6)
+    minHeap.push(22)
+    minHeap.push(9)
 
-    minHeap.Print()
-    print("The Min val is " + str(minHeap.remove()))
+    print(minHeap.pop())
+    print(minHeap.pop())
+    print(minHeap.pop())
+    print(minHeap.pop())
+    print(minHeap.pop())
+    print(minHeap.pop())
+    print(minHeap.pop())
