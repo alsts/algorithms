24ce9ce47874a37a4e8454099107db285f33be0c
Aleksandr Stserbatski <astserba@gmail.com>
2024-06-05 11:47:02 +0300

    Matrix DFS

--- neetcode/course/matrix/matrix_dfs.py
+++ neetcode/course/matrix/matrix_dfs.py
@@ -1,23 +1,26 @@
-# count paths (backtracking)
-def dfs(grid, r, c, visit):
+# count paths (brute force DFS backtracking)
+def dfs(grid, r, c, visited):
     ROWS, COLS = len(grid), len(grid[0])
+    out_of_bounds = min(r, c) < 0 or r == ROWS or c == COLS
 
-    if (min(r, c) < 0 or
-            r == ROWS or c == COLS or
-            (r, c) in visit or grid[r][c] == 1):
+    # Base Cases
+    # Traversal -> [out of bounds, already visited, blocked path]:
+    if out_of_bounds or (r, c) in visited or grid[r][c] == 1:
         return 0
+    # Reached destination [bottom right]
     if r == ROWS - 1 and c == COLS - 1:
         return 1
 
-    visit.add((r, c))
+    visited.add((r, c))  # add to visited -> only true for one path, would be removed on backtracking to count all available paths
 
     count = 0
-    count += dfs(grid, r + 1, c, visit)  # right
-    count += dfs(grid, r - 1, c, visit)  # left
-    count += dfs(grid, r, c + 1, visit)  # up
-    count += dfs(grid, r, c - 1, visit)  # down
+    # Recursive calls to traverse and count all available path to Destination in 4 directions:
+    count += dfs(grid, r + 1, c, visited)  # right
+    count += dfs(grid, r - 1, c, visited)  # left
+    count += dfs(grid, r, c + 1, visited)  # up
+    count += dfs(grid, r, c - 1, visited)  # down
 
-    visit.remove((r, c))
+    visited.remove((r, c))  # After node directions recursively processed -> backtrack
     return count
 
 
