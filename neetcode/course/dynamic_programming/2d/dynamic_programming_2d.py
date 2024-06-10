2b4d160a59ad329057f30508b9f1e9f8bb7e8403
Aleksandr Stserbatski <astserba@gmail.com>
2024-06-10 16:31:20 +0300

    2D Dynamic Programming

--- neetcode/course/dynamic_programming/2d/dynamic_programming_2d.py
+++ neetcode/course/dynamic_programming/2d/dynamic_programming_2d.py
@@ -1,9 +1,11 @@
 def dp_bottom_up(rows, cols):
     prev_row = [0] * cols
 
+    # traverse through cells bottom up:
     for r in range(rows - 1, -1, -1):  # from, to, step
         cur_row = ([0] * (cols - 1)) + [1]  # last item is always 1
 
+        # calculate row cells -> move right to left, ignore rightmost item as it is always 1
         for c in range(cols - 2, -1, -1):
             cur_row[c] = cur_row[c + 1] + prev_row[c]  # sum values from right and bottom cells
         prev_row = cur_row
