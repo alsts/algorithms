0fac676b7c2b3fc4e6fc0717d19eafeca7be539b
Alex <astserba@gmail.com>
2024-06-29 18:07:40 +0100

    Tweaks

--- neetcode/course/graph/adjacency_list_bfs.py
+++ neetcode/course/graph/adjacency_list_bfs.py
@@ -25,15 +25,16 @@ def bfs(start, finish, adj_list):
     path_count = 0
 
     while queue:
-        curr = queue.popleft()
-        if curr == finish:
-            return path_count
+        # take snapshot of layer, loop through all layer nodes
+        for i in range(len(queue)):
+            curr = queue.popleft()
+            if curr == finish:
+                return path_count
 
-        for vertex in adj_list[curr]:
-            if vertex not in visited:
-                queue.append(vertex)
-                visited.add(vertex)
-
+            for vertex in adj_list[curr]:
+                if vertex not in visited:
+                    queue.append(vertex)
+                    visited.add(vertex)
         path_count += 1
 
     return path_count
